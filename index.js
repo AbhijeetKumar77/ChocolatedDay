aa2801b9f1fccebc64a27605fd381cf67d7e5a12
AbhijeetKumar77 <154896374+AbhijeetKumar77@users.noreply.github.com>
2024-02-09 04:22:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let text = "The sweetness of these chocolates is nothing compared to that one smile of yours. Happy Chocolate Day!";
+let i = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById('walking-text').innerHTML += text.charAt(i);
+        document.getElementById('walking-text2').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 50);
+    }
+}
+typeWriter();
